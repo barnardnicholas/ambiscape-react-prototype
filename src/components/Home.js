d50524b7b370ba26a8d0efb9bad3da8746d4b1fc
Nick <barnard.nicholas@googlemail.com>
2020-02-03 11:42:04 +0000

    tweak layouts on home screen

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,12 @@ import Header from "./Header";
 import { Link } from "@reach/router";
 
 const Home = () => {
+  const pageStyle = {
+    display: "flex",
+    flexDirection: "column"
+    // alignContent: "flex-end"
+  };
+
   const styling = {
     margin: 0,
     padding: "6px",
@@ -10,17 +16,27 @@ const Home = () => {
   };
 
   const listStyle = {
-    position: "50% 50%",
-    margin: "8px 16px",
+    // position: "50% 50%",
+    margin: "0px 16px 0px 16px",
     padding: "0px",
     width: "80%",
     maxWidth: "300px",
-    listStyle: "none"
+    listStyle: "none",
+    alignSelf: "center"
+  };
+
+  const titleSyle = {
+    // margin: "180px 0px 16px 0px",
+    fontSize: "80px",
+    opacity: "0.7",
+    textShadow: "#ffffff 0 0 0.8rem",
+    alignContent: "flex-center",
+    justifyContent: "flex-center"
   };
 
   const backgroundStyling = {
     position: "absolute",
-    width: "76%",
+    width: "80%",
     height: "56px",
     margin: "0px",
     padding: "0px",
@@ -33,21 +49,37 @@ const Home = () => {
     maxWidth: "300px"
   };
   return (
-    <>
-      <Header headerText="Ambiscape" />
-      <center>
-        <ul style={listStyle}>
-          <Link to="/scenarios" key="scenarios" className="reactlink">
-            <li>
-              <div style={backgroundStyling}></div>
-              <div style={styling}>
-                <h3>Preset Scenarios</h3>
-              </div>
-            </li>
-          </Link>
-        </ul>
-      </center>
-    </>
+    <center style={pageStyle}>
+      <header style={titleSyle}>
+        <h2 style={{ fontSize: "80px" }}>Ambiscape</h2>
+      </header>
+      <ul style={listStyle}>
+        <Link to="/scenarios" key="scenarios" className="reactlink">
+          <li>
+            <div style={backgroundStyling}></div>
+            <div style={styling}>
+              <h3>Preset Scenarios</h3>
+            </div>
+          </li>
+        </Link>
+        <Link to="/login" key="login" className="reactlink">
+          <li>
+            <div style={backgroundStyling}></div>
+            <div style={styling}>
+              <h3>Login</h3>
+            </div>
+          </li>
+        </Link>
+        <Link to="/signup" key="signup" className="reactlink">
+          <li>
+            <div style={backgroundStyling}></div>
+            <div style={styling}>
+              <h3>Sign Up</h3>
+            </div>
+          </li>
+        </Link>
+      </ul>
+    </center>
   );
 };
 
