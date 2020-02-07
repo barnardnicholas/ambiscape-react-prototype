74a113c71174b9ac8b887b196f2a4ad1f54ca919
Nick <barnard.nicholas@googlemail.com>
2020-02-07 09:16:16 +0000

    test html5 bug on ios

--- src/utils/audio-engine.js
+++ src/utils/audio-engine.js
@@ -14,8 +14,8 @@ const createHowl = (channel, url) => {
   const howlData = {
     src: [filePath],
     stereo: stereo,
-    loop: loop,
-    html5: false
+    loop: loop
+    // html5: false
   };
   // if (type === "background") {
   //   howlData.html5 = true;
